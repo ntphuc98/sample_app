5e8aef2c18385cb8e8fd012aa9e16edf81496cc1
Nguyen Thanh Phuc <ntphuc98it@gmail.com>
2019-08-01 17:16:19 +0700

    Chapter 4: Rails-flavored Ruby

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  # Returns the full title on a per-page basis.
+  def full_title page_title
+    base_title = I18n.t ".layouts.application.title"
+    sub_title = I18n.t ".layouts.application._title"
+    page_title.blank? ? base_title : page_title + sub_title + base_title
+  end
 end
