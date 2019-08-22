3415b73938ebe62ed96851cfa9543cef18f5ca5c
ntphuc98 <ntphuc98it@gmail.com>
2019-08-22 09:08:26 +0700

    Chapter 11: Account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "sendmailphp2018@gmail.com"
   layout "mailer"
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,28 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :test
+
+  host = "localhost:3000" # Don't use this literally; use your local dev host instead
+
+  # Use this on the cloud IDE.
+  # config.action_mailer.default_url_options = { host: host, protocol: "https" }
+  # Use this if developing on localhost.
+  config.action_mailer.default_url_options = { host: host, protocol: "http" }
+
+  config.action_mailer.delivery_method = :smtp
+
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    domain: "gmail.com",
+    enable_starttls_auto: true
+  }
 end
